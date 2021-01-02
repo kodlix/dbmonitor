1a3df5f4e3785083e251b7376754efc719f9e219
Johnson Iyida <chikajohnson@gmail.com>
2021-01-02 03:35:53 +0100

    add link to download db metric as excel file

--- web/src/Monitor.js
+++ web/src/Monitor.js
@@ -20,16 +20,21 @@ const Monitor = () => {
                     {/* <label className="pr-2">Select Period: </label> */}
                     {/* <select disabled>
                         <option>Today</option> */}
-                        {/* <option>This Week</option>
+                    {/* <option>This Week</option>
               <option>This Month</option> */}
                     {/* </select> */}
                     <label className="pl-2 font-weight-bold">Today: &nbsp;{new Date().toUTCString().substring(0, 25)}</label>
                     <hr className="py-4" />
                     <table className="table text-left ">
+                        <caption className="text-right font-weight-bold text-secondary">Download database metrics in excel format :
+                        <a className="text-primary" href="#">
+                                <i class="bi bi-download px-1"></i>
+                                database.xls
+                        </a></caption>
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th  scope="col">Database Name</th>
+                                <th scope="col">Database Name</th>
                                 <th scope="col">Size Yestarday (mb)</th>
                                 <th scope="col">Current Size (mb)</th>
                                 <th scope="col">Daily Growth Rate (%)</th>
@@ -37,7 +42,7 @@ const Monitor = () => {
                         </thead>
                         <tbody>
                             {
-                                db.data &&  db.data.length > 0 && db.data.map((item, index) =>                               
+                                db.data && db.data.length > 0 && db.data.map((item, index) =>
                                     <tr key={item.id}>
                                         <th scope="row">{index + 1} {console.log(item)}</th>
                                         <td className="text-uppercase font-weight-bold">{item.db_name}</td>
