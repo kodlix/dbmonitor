55e1f916d9028508fe03374f2a9c3325a8a317bb
Johnson Iyida <kodili.johnson@gmail.com>
2021-01-05 09:47:44 +0100

    add dynamic title

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,12 @@
+import {Helmet} from 'react-helmet'
+
 import Monitor from './Monitor';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet title={'Netop DB Monitorcd'} />
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
         <a className="navbar-brand ml-4 pr-4" href="#">Netop DB Monitor</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
